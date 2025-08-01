060394ff9ecaac962f41c1074e7ddf2c0b5fb33e
CaseyNelson314 <caseynelson314@gmail.com>
2025-08-01 19:41:21 +0900

    websocketの受信コードちょい書き

--- core_daemon/main.cpp
+++ core_daemon/main.cpp
@@ -1,5 +1,7 @@
 #include <unistd.h>
 #include <iostream>
+#include <App.h>
+
 
 #include "gpio.hpp"
 #include "servo.hpp"
@@ -25,27 +27,52 @@ int main()
     //led.begin();
     //mecanum.begin();
 
-    for (;;)
-    {
-	hand.move(0);
-	sleep(1);
-	hand.move(deg_to_rad(270));
-	sleep(1);
-        // led.write(true);
-        // std::cout << "hi\n";
-        // sleep(1);
+    // for (;;)
+    // {
+	// hand.move(0);
+	// sleep(1);
+	// hand.move(deg_to_rad(270));
+	// sleep(1);
+    //     // led.write(true);
+    //     // std::cout << "hi\n";
+    //     // sleep(1);
 
-        // led.write(false);
-        // std::cout << "lo\n";
-        // sleep(1);
+    //     // led.write(false);
+    //     // std::cout << "lo\n";
+    //     // sleep(1);
 
-		// hand.move(0);
-        // sleep(1);
-		// hand.move(M_PI);
-        // sleep(1);
+	// 	// hand.move(0);
+    //     // sleep(1);
+	// 	// hand.move(M_PI);
+    //     // sleep(1);
 
-		mecanum.move(0.5, 0.5, 0);
-    }
+	// 	mecanum.move(0.5, 0.5, 0);
+    // }
+    struct UserData
+    {};
+
+    uWS::App().ws<UserData>("/", {
+        .open = [](auto */*ws*/) {
+            std::cout << "[ OK ] client connected\n";
+        },
+        .message = [](auto *ws, std::string_view msg, uWS::OpCode opCode) {
+            try {
+                std::cout << "[ OK ] " << msg << std::endl;
+            } catch (...) {
+                std::cerr << "[ NG ] json invalid.\n";
+            }
+            ws->send(msg, opCode, false);
+        },
+        .close = [](auto */*ws*/, int /*code*/, std::string_view /*message*/) {
+            std::cout << "[ OK ] client disconnected\n";
+        }
+    }).listen(9001, [](auto *token) {
+        if (token) {
+            std::cout << "[ OK ] WebSocket server activation: ws://raspberrypi.local:9001\n";
+        } else {
+            std::cerr << "[ NG ] Port unavailable\n";
+        }
+    }).run();
 
     return 0;
 }
