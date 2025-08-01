9e4f023b98f05a29d1d7cedbf49bbbc8220ae9b0
CaseyNelson314 <caseynelson314@gmail.com>
2025-08-02 01:21:53 +0900

    websocket と メカナムのドライバを接続

--- core_daemon/main.cpp
+++ core_daemon/main.cpp
@@ -10,18 +10,19 @@
 #include "websocket.hpp"
 #include "json_parser.hpp"
 
-gpio_enabler enabler;
+static gpio_enabler enabler;
 
-pin_output led{ 2 };
-
-servo hand{ 17, deg_to_rad(270), { 500, 2500 } };
-
-mecanum_wheel mecanum {{
+static mecanum_wheel mecanum {{
 	motor{ pin_output{ 1 }, pin_output{ 2 }, pin_pwm{ 3 } },
 	motor{ pin_output{ 1 }, pin_output{ 2 }, pin_pwm{ 3 } },
 	motor{ pin_output{ 1 }, pin_output{ 2 }, pin_pwm{ 3 } },
 	motor{ pin_output{ 1 }, pin_output{ 2 }, pin_pwm{ 3 } },
 }};
+
+static servo axis1{ 17, deg_to_rad(270), { 500, 2500 } };
+static servo axis2{ 18, deg_to_rad(270), { 500, 2500 } };
+static servo axis3{ 19, deg_to_rad(270), { 500, 2500 } };
+static servo axis4{ 20, deg_to_rad(270), { 500, 2500 } };
 
 
 int main(int argc, char** argv)
@@ -30,31 +31,6 @@ int main(int argc, char** argv)
         return 1;
 
     const int websocket_server_port = std::atoi(argv[1]);
-
-    //led.begin();
-    //mecanum.begin();
-
-    // for (;;)
-    // {
-	// hand.move(0);
-	// sleep(1);
-	// hand.move(deg_to_rad(270));
-	// sleep(1);
-    //     // led.write(true);
-    //     // std::cout << "hi\n";
-    //     // sleep(1);
-
-    //     // led.write(false);
-    //     // std::cout << "lo\n";
-    //     // sleep(1);
-
-	// 	// hand.move(0);
-    //     // sleep(1);
-	// 	// hand.move(M_PI);
-    //     // sleep(1);
-
-	// 	mecanum.move(0.5, 0.5, 0);
-    // }
 
     websocket_server_start(websocket_server_config{
         .server_port = websocket_server_port,
@@ -75,15 +51,24 @@ int main(int argc, char** argv)
         .on_message = [](std::string_view message) -> std::string {
             if (const auto receive_data = parse_json(message))
             {
-                std::cout
-                    << receive_data->wheel.x << " "
-                    << receive_data->wheel.y << " "
-                    << receive_data->wheel.turn << "\n";
-                std::cout
-                    << receive_data->arm.axis1 << " "
-                    << receive_data->arm.axis2 << " "
-                    << receive_data->arm.axis3 << " "
-                    << receive_data->arm.axis4 << "\n";
+                auto mecanum_power = receive_data->wheel;
+                mecanum.move(mecanum_power.x, mecanum_power.y, mecanum_power.turn);
+
+                auto arm_power = receive_data->arm;
+                axis1.move(arm_power.axis1);
+                axis2.move(arm_power.axis2);
+                axis3.move(arm_power.axis3);
+                axis4.move(arm_power.axis4);
+                // std::cout
+                //     << mecanum_power.x << " "
+                //     << mecanum_power.y << " "
+                //     << mecanum_power.turn << "\n";
+                
+                // std::cout
+                //     << receive_data->arm.axis1 << " "
+                //     << receive_data->arm.axis2 << " "
+                //     << receive_data->arm.axis3 << " "
+                //     << receive_data->arm.axis4 << "\n";
                 return "[ OK ]";
             }
             else
@@ -92,7 +77,4 @@ int main(int argc, char** argv)
             }
         }
     });
-
-    // { "wheel": { "x":0, "y":1, "turn":1 }, "arm": { "axis1":0, "axis2":0, "axis3":1, "axis4":1 }  }
-    // { "wheel": { "x": 0.5, "y": 0.5, "turn": 0.1 }, "arm": { "axis1": 3.14, "axis2": 3.14, "axis3": 3.14, "axis4": 3.14 } }
 }
